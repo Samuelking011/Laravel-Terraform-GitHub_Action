57a67f508e4a5feb172162591fb5414f40a09021
Samuel Olayinka <olayinkasamuel44@gmail.com>
2023-10-11 19:58:23 -0700

    initial commit

--- terraform.tfvars
+++
@@ -1,3 +0,0 @@
-aws_access_key = "AWS Access Key"
-aws_secret_key = "AWS Secret key"
-aws_region = "us-east-1"
--- variables.tf
+++ variables.tf
@@ -1,15 +1,8 @@
 # variables.tf
 
-variable "aws_access_key" {
-    description = "The IAM public access key"
-}
-
-variable "aws_secret_key" {
-    description = "IAM secret access key"
-}
-
 variable "aws_region" {
     description = "The AWS region things are created in"
+    default = "us-east-1"
 }
 
 variable "app_name" {
